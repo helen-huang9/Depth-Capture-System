ef28a49f7adee21243010dc80048ffb95942071a
Helen Huang <helen_huang@brown.edu>
2022-04-26 22:15:29 -0400

    Initial Commit

---
+++ Depth Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Depth Map
+//
+//  Created by Helen Huang on 4/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Depth Map/Depth_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Depth_MapApp.swift
+//  Depth Map
+//
+//  Created by Helen Huang on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Depth_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
